f40c830d10544c7e8fe631ffbb29118bab16c441
Felipe Santos <felipe.santos@qualyteam.com.br>
2024-12-09 11:01:45 -0300

    Add: Radix Radio Group to TransactionTypeButtons

--- src/components/NewTransactionModal/styles.ts
+++ src/components/NewTransactionModal/styles.ts
@@ -1,5 +1,6 @@
 import styled from "styled-components";
 import * as Dialog from "@radix-ui/react-dialog";
+import * as RadioGroup from "@radix-ui/react-radio-group";
 
 export const Overlay = styled(Dialog.Overlay)`
     position: fixed;
@@ -68,7 +69,7 @@ export const Content = styled(Dialog.Content)`
     }
 `;
 
-export const TransactionType = styled.div`
+export const TransactionType = styled(RadioGroup.Root)`
     display: grid;
     grid-template-columns: repeat(2, 1fr);
     gap: 1rem;
@@ -80,7 +81,7 @@ interface TransactionTypeButtonProps {
     variant: "income" | "outcome";
 }
 
-export const TransactionTypeButton = styled.button<TransactionTypeButtonProps>`
+export const TransactionTypeButton = styled(RadioGroup.Item)<TransactionTypeButtonProps>`
     display: flex;
     align-items: center;
     justify-content: center;
@@ -98,6 +99,20 @@ export const TransactionTypeButton = styled.button<TransactionTypeButtonProps>`
 
     svg {
         color: ${props => props.variant === "income" ? props.theme["green-300"] : props.theme["red-300"]};
+    }
+
+    &[data-state="unchecked"]:hover {
+        background-color: ${props => props.theme["gray-600"]};
+        transition: background-color 0.2s;
+    }
+
+    &[data-state="checked"] {
+        color: ${props => props.theme.white};
+        background-color: ${props => props.variant === "income" ? props.theme["green-500"] : props.theme["red-500"]};
+
+        svg {
+            color: ${props => props.theme.white};
+        }
     }
 `;
 
