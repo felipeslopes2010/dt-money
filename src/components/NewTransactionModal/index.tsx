f480fe014442cd5653763aead92ca18c18836c4c
Felipe Santos <felipe.santos@qualyteam.com.br>
2024-12-14 20:36:53 -0300

    Adjust: Uses Axios Post in creation of a new Transaction

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,4 +1,6 @@
+import { useContext } from "react";
 import { Controller, useForm } from "react-hook-form";
+import { TransactionsContext } from "../../contexts/TransactionsContext";
 import * as z from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
 import * as Dialog from "@radix-ui/react-dialog";
@@ -16,11 +18,14 @@ const newTransactionFormSchema = z.object({
 type NewTransactionFormInputs = z.infer<typeof newTransactionFormSchema>;
 
 export function NewTransactionModal() {
+    const { createTransaction } = useContext(TransactionsContext);
+    
     const {
         control,
         register,
         handleSubmit,
-        formState: { isSubmitting }
+        formState: { isSubmitting },
+        reset,
     } = useForm<NewTransactionFormInputs>({
         resolver: zodResolver(newTransactionFormSchema),
         defaultValues: {
@@ -29,8 +34,16 @@ export function NewTransactionModal() {
     });
 
     async function handleCreateNewTransaction(data: NewTransactionFormInputs) {
-        await new Promise(resolve => setTimeout(resolve, 2000));
-        console.log(data)
+        const { description, price, category, type } = data;
+
+        await createTransaction({
+            description,
+            price,
+            category,
+            type,
+        });
+
+        reset();
     }
 
     return (
