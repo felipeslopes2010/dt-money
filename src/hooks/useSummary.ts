aa4fb37c6314337004d1507f8e8f5aab4043871d
Felipe Santos <felipe.santos@qualyteam.com.br>
2024-12-15 16:13:20 -0300

    Adjust: useMemo to Summary

--- src/hooks/useSummary.ts
+++ src/hooks/useSummary.ts
@@ -1,3 +1,4 @@
+import { useMemo } from "react";
 import { useContextSelector } from "use-context-selector";
 
 import { TransactionsContext } from "../contexts/TransactionsContext";
@@ -7,18 +8,20 @@ export function useSummary() {
         return context.transactions;
     });
 
-    const summary = transactions.reduce((acc, transaction) => {
-        if(transaction.type === "income") {
-            acc.income += transaction.price;
-        } else {
-            acc.outcome += transaction.price;
-        }
+    const summary = useMemo(() => {
+        return transactions.reduce((acc, transaction) => {
+            if (transaction.type === "income") {
+                acc.income += transaction.price;
+            } else {
+                acc.outcome += transaction.price;
+            }
 
-        acc.total = acc.income - acc.outcome;
-        return acc;
-    }, {
-        income: 0, outcome: 0, total: 0
-    });
+            acc.total = acc.income - acc.outcome;
+            return acc;
+        }, {
+            income: 0, outcome: 0, total: 0
+        });
+    }, [transactions]);
 
     return summary;
-}
+};
